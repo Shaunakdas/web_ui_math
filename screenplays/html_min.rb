e0017fcba73a8940c4ab5036f38e2b0f05fe0cc3
Shaunak Das <shaunakdas2020@gmail.com>
2017-12-25 19:46:51 +0530

    removed old script and html files

--- screenplays/html_min.rb
+++
@@ -1,7 +0,0 @@
-require 'html_press'
-require 'nokogiri'
-file = File.open("QuestionData_6.html", "r")
-content = file.read
-content =  HtmlPress.press(content)
-@doc = Nokogiri::XML(content)
-puts HtmlPress.press(@doc.xpath("//body")[0].attr("code"))
